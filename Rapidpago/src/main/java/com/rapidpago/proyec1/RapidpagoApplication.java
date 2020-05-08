0d5929cfa42a2756f7b6c654e33114885ce53fe9
Usuario <Usuario@ANDRES-SALAPC>
2020-05-08 13:21:34 -0400

    primer commit

---
+++ Rapidpago/src/main/java/com/rapidpago/proyec1/RapidpagoApplication.java
@@ -0,0 +1,13 @@
+package com.rapidpago.proyec1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RapidpagoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RapidpagoApplication.class, args);
+	}
+
+}
---
+++ Rapidpago/src/test/java/com/rapidpago/proyec1/RapidpagoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rapidpago.proyec1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RapidpagoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
